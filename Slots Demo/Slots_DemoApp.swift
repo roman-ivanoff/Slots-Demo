5f573bf806af4b2ee245ce4d7a178ae1821323b9
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-01-17 14:44:08 +0200

    Initial Commit

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Roman Ivanov on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
